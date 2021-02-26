62f12255b5610e50ee1233003c052cf03cb131f4
Adam <57396704+Sankti@users.noreply.github.com>
2021-02-26 19:07:52 +0100

    Testing rooting in Express

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/profile", (req, res) => {
     const user = {
         name: "John",
         hobby: "Ornithology"
     }
 
     res.send(user)
+})
+
+app.get("/", (req, res) => {
+    res.send("<h1>Root Directory</h1>")
 })
 
 app.listen(3000);
