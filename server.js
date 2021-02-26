ee17b73e04af409152200ecd463d1c1eb58944b6
Adam <57396704+Sankti@users.noreply.github.com>
2021-02-26 18:48:20 +0100

    Testing responding with JSON

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.setHeader("Content-Type", "text/html");
-    response.end("<h1>Hello. Thank you for the request.</h1>")
+    console.log("headers", request.headers);
+    console.log("method", request.method);
+    console.log("url", request.url);
+
+    const user = {
+        name: "John",
+        hobby: "Ornithology"
+    }
+
+    response.setHeader("Content-Type", "application/json");
+    response.end(JSON.stringify(user));
 });
 
 server.listen(3000);
