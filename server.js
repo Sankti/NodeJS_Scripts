0b916bcf61367900374a186ee249311c946021e8
Adam <57396704+Sankti@users.noreply.github.com>
2021-02-26 19:33:20 +0100

    Testing GET & POST methods using Express Middleware
    Tested with Postman

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("<h1>Root Directory</h1>");
+});
+
 app.get("/profile", (req, res) => {
+    console.log(req.body);
     const user = {
         name: "John",
         hobby: "Ornithology"
-    }
+    };
 
-    res.send(user)
-})
+    res.send(user);
+});
 
-app.get("/", (req, res) => {
-    res.send("<h1>Root Directory</h1>")
-})
+app.post("/profile", (req, res) => {
+    console.log(req.body);
+    res.send("Success");
+});
 
 app.listen(3000);
