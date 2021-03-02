a2e31f76dd805caaacbc95b83355268c7533ee64
Adam <57396704+Sankti@users.noreply.github.com>
2021-03-02 23:10:25 +0100

    Update simpleProblems.js

--- misc/simpleProblems.js
+++ misc/simpleProblems.js
@@ -8,19 +8,19 @@
 // isPalindrome("Wieniec");
 // isPalindrome("Kajak");
 
-// // Fibonacci 1
-// const fibonacci = number => {
-//     const result = [0, 1]
+// Fibonacci 1
+const fibonacci = number => {
+    const fibonacciSequence = [0, 1]
 
-//     for (let i = 2; i <= number; i++) {
-//         const previousNumber1 = result[i - 1];
-//         const previousNumber2 = result[i - 2];
-//         result.push(previousNumber1 + previousNumber2);
-//     };
+    for (let i = 2; i <= number; i++) {
+        const previousNumber1 = fibonacciSequence[i - 1];
+        const previousNumber2 = fibonacciSequence[i - 2];
+        fibonacciSequence.push(previousNumber1 + previousNumber2);
+    };
     
-//     console.log(result[number]);
-// };
-// fibonacci(9);
+    console.log(fibonacciSequence[number]);
+};
+fibonacci(9);
 
 // // Fibonacci 2 - Recursive
 // const fibonacciRecursive = number => {
@@ -31,21 +31,21 @@
 // };
 // console.log(fibonacciRecursive(9));
 
-const fizzBuzz = num => {
-    for(let i = 1; i <= num; i++) {
-        // check if the number is a multiple of 3 and 5
-        if(i % 3 === 0 && i % 5 === 0) {
-        console.log('fizzbuzz')
-        } // check if the number is a multiple of 3
-        else if(i % 3 === 0) {
-        console.log('fizz')
-        } // check if the number is a multiple of 5
-        else if(i % 5 === 0) {
-        console.log('buzz')
-        } else {
-        console.log(i)
-        }
-    }
-}
+// const fizzBuzz = num => {
+//     for(let i = 1; i <= num; i++) {
+//         // check if the number is a multiple of 3 and 5
+//         if(i % 3 === 0 && i % 5 === 0) {
+//         console.log('fizzbuzz')
+//         } // check if the number is a multiple of 3
+//         else if(i % 3 === 0) {
+//         console.log('fizz')
+//         } // check if the number is a multiple of 5
+//         else if(i % 5 === 0) {
+//         console.log('buzz')
+//         } else {
+//         console.log(i)
+//         }
+//     }
+// }
 
-fizzBuzz(15)
+// fizzBuzz(15)
