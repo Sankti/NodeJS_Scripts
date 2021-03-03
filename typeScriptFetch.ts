46892f96812f258942806359a8f127c6a881f367
Adam <57396704+Sankti@users.noreply.github.com>
2021-03-03 17:20:01 +0100

    Testing types' functionality in TS

--- typeScriptFetch.ts
+++ typeScriptFetch.ts
@@ -13,16 +13,20 @@ interface Todo {
 
 // Asynchronous promise:
 axios.get(url).then(response => {
-    const todo = response.data;
+    const todo = response.data as Todo;
 
     const id = todo.id;
     const title = todo.title;
     const completed = todo.completed;
 
 
+    logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
     Todo ID: ${id}
     Title: ${title}
     Has it been finished: ${completed}
-    `)
-});
+    `);
+};
