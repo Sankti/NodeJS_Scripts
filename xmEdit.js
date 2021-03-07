3e49821739608bc9ffb744a7d4dc75fe9c735380
Adam <57396704+Sankti@users.noreply.github.com>
2021-03-07 17:40:34 +0100

    Attempting to parse an XML file

---
+++ xmEdit.js
@@ -0,0 +1,11 @@
+var fs = require("fs"),
+parseString = require("xml2js").parseString;
+
+fs.readFile("reed.xml", "utf-8", function(err, data) {
+    if (err) console.log(err);
+        console.log(data);
+    parseString(data, function(err, result) {
+        if (err) console.log(err);
+        console.log(result);
+    });
+});
